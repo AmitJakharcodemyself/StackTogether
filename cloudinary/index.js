5260068243e698b05eceea92fa361ad818bfcadc
AmitJakharcodemyself <amit7665253807@gmail.com>
2021-06-29 19:41:15 +0530

    API Key

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,5 +1,11 @@
+if(process.env.NODE_ENV !=='production'){
+    require('dotenv').config();
+}
+
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
